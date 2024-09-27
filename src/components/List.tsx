d2ff62f6621260e7dea3400134079af257a9bed7
Suyel Bishnu <suyel2007@gmail.com>
2024-09-27 14:46:48 +0300

    Calculate the  total price of the products

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../hooks/hooks";
 import { Product } from "./Product";
 import { fetchProducts } from "../features/productSlice";
+import { calculateTotalOfProducts } from "../utils/utils";
 
 const List = () => {
   const products = useAppSelector((state) => state.products.products);
@@ -10,9 +11,12 @@ const List = () => {
   useEffect(()=>{
     dispatch(fetchProducts());
   }, [dispatch]);
+
+  const totalOfProducts = calculateTotalOfProducts(products)
   return (
     <div>
       <h1>List will be here</h1>
+      <h2>{totalOfProducts}</h2>
       {products.map((product)=>(
         <Product {...product} />
       ))}
