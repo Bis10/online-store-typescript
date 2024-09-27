24eabc26b4e6433bd0116459a849b0744ba605c2
Suyel Bishnu <suyel2007@gmail.com>
2024-09-27 14:49:19 +0300

    Create a function to calculate total price

---
+++ src/utils/utils.ts
@@ -0,0 +1,31 @@
+import { Product } from "../features/productSlice";
+
+export const calculateTotalOfProducts = (products: Product[])=>{
+    // This tells typescript that total is always a number
+    // let total: number = 0;
+    let total: number= 0;
+    products.forEach((product)=>{
+        total += product.price
+    })
+    return parseFloat(total.toFixed(2));
+}
+
+// export const reverseStringToUpperCase=(string:string)=>{
+//     return string.split("").reverse().join("").toUpperCase();
+// }
+// const result = reverseStringToUpperCase('Hello');
+// console.log(result); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
