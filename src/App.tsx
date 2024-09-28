2e0abf66e75fecee595054906f1a5c2eeb55a305
Suyel Bishnu <suyel2007@gmail.com>
2024-09-28 09:39:59 +0300

    Add the route path

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import StoreNavbar from './components/StoreNavbar';
 import List from './components/List';
+import StringReverse from './components/StringReverse';
 
 function App() {
   
 
   return (
-   
-      <div>
-       <StoreNavbar/>
-       <BrowserRouter>
-       <Routes>
-        <Route path = "/" element={<List/>} />
-       </Routes>
-       </BrowserRouter>
-      </div>
-   
+    <div>
+      <StoreNavbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<List />} />
+          <Route path="/string" element={<StringReverse />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
